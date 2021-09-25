f15f5a8912e12e238dab6d97a4f6b4b882f16886
colson <dud3722000@naver.com>
2021-09-25 18:28:15 +0900

    Update README

--- README.md
+++ README.md
@@ -4,9 +4,24 @@ Voice assistant & Home IoT
 ```Create low cost home-iot server and application.```
 
 ### TODO LIST
-* chatbot(custom)
-* hotword detection
+* chatbot(wit ai)
+* hotword detection (snowboy)
 * beacon(i-beacon)
+  * localization (poor resolution)
+* Face recognition
 * home server
 * motion detection (IR)
-* localization (poor resolution)
+* RaspberryPi zero W housing (or RaspberryPi4)
+* RaspberryPi Shield (AC power control & IO control)
+* MCU build (IoT Device)
+
+
+### Target Deadline
+**2021.12.31**
+### Schedule
+#### Week 1
+* Make message conversation (using: [wit.ai](https://wit.ai/), [snowboy](https://github.com/Kitt-AI/snowboy))
+* 
+#### Week 2
+* Search: On/Off controller
+* 
--- app/hotword_detection.py
+++ app/hotword_detection.py
@@ -8,13 +8,23 @@ interrupted = False
 
 def signal_handler(signal, frame):
     global interrupted
+
+    print("signal haldler")
+    print(frame)
+    print(signal)
+
     interrupted = True
 
 
+def detected_callback():
+    print("Detected callback called")
+
+
 def interrupt_callback():
-    print(f"Jarvis called")
+    # print(f"Jarvis called")
     global interrupted
     return interrupted
+
 
 if len(sys.argv) == 1:
     print("Error: need to specify model name")
@@ -26,11 +36,11 @@ model = sys.argv[1]
 # capture SIGINT signal, e.g., Ctrl+C
 signal.signal(signal.SIGINT, signal_handler)
 
-detector = snowboydecoder.HotwordDetector(model, sensitivity=[0.8, 0.80])
+detector = snowboydecoder.HotwordDetector(model, sensitivity=0.8)
 print('Listening... Press Ctrl+C to exit')
 
 # main loop
-detector.start(detected_callback=snowboydecoder.play_audio_file,
+detector.start(detected_callback=detected_callback,
                interrupt_check=interrupt_callback,
                sleep_time=0.03)
 
